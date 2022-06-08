d5d4f55364198bc78dd8b4a3eab561352df6c994
marinoL98 <62379689+marinoL98@users.noreply.github.com>
2022-06-08 11:17:29 +0200

    Add files via upload

--- tablica.php
+++ tablica.php
@@ -17,9 +17,15 @@
 
       
 
-      body{
-        overflow-x: hidden;
-      }
+    body{
+      overflow-x: hidden;
+    }
+
+    #navigacija{
+      height:80px;
+
+    }
+    
     #inputi{
         align-content:center;
         width:70%;
@@ -49,29 +55,30 @@
       list-style: none;
       color:black;
     }
-    button:active{
-        
+    body{
+        transition-duration: 0.5s;
+    }
+
+    #toggle{
+      color:black;
     }
     </style>
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top"id="navigacija">
-            <div class="container">
-              <ul class="navbar-nav mx-auto sm-12 md-4"id="mojUl"> <!--MX-AUTO CENTRIRA postoje jos 'ms' i sl.....-->
-                  <h5><li class="nav-item active "id="mojLi"><a class="nav-link" href="#">Home</a></li></h5>
-                  <h5><li class="nav-item active "id="mojLi"><a class="nav-link" href="#">Library</a></li></h5>
-                  <h5><li class="nav-item active "id="mojLi"><a class="nav-link" href="#">Play</a></li></h5>
-                  
-
-
-              </ul>  
-              <li class="nav-item active "id="dn"><a class="nav-link"><h3><i class="bi bi-brightness-high-fill"id="toggle"></i></h3></li>
-              
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top"id="navigacija">
+        <div class="container">
+          <ul class="navbar-nav mx-auto sm-12 md-4"id="mojUl"> <!--MX-AUTO CENTRIRA postoje jos 'ms' i sl.....-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <li class="nav-item active "id="mojLi"><a class="nav-link" href="index.html">Index</a></li>
+            <li class="nav-item active "id="mojLi"><a class="nav-link" href="tablica.php">Pjesme</a></li>
+            <li class="nav-item active "id="mojLi"><a class="nav-link" href="simulator.html">Sviraj</a></li>
+            <li class="nav-item active "id="mojLi"><a class="nav-link" href="kviz.html">Kviz</a></li>
+          </ul>  
+          <li class="nav-item active "id="dn"><a class="nav-link"><h3><i class="bi bi-brightness-high-fill"id="toggle"></i></h3></li>
+        </div>
     </nav>
 
-  <form action=""method="GET">
+  <form action=""method="POST">
     <div class="row"id="inputi">
            <div class="col-3"> <input type="text" class="form-control" placeholder="Pjesma" id="form1"name="in1"></div>
             <div class="col-2"><input type="text" class="form-control" placeholder="Izvođač" id="form2"name="in2"></div>
@@ -79,20 +86,21 @@
             <div class="col-1"><input type="number" class="form-control" placeholder="Godina" id="form4"name="in4"></div>
             <div class="col-3"><button type="button" class="btn btn-dark"id="add"name="bt"><b>+</b></button>
             <button type="button" class="btn btn-dark"id="ispis"name="ispis"><b><i class="bi bi-card-list"></i></b></button>
-            <button type="submit" class="btn btn-dark"id="ispis"name="sv"><b><i class="bi bi-save2-fill"></i></b></button></div>        
+            <button type="submit" class="btn btn-dark"id="save"name="sv"><b><i class="bi bi-save2-fill"></i></b></button></div>        
           </div>
     </div>
   </form>
     <br><br><br><br>
 
     <div class="row"id="content">
-        <table class="table">
+        <table class="table"id="mojtab">
             <thead class="thead-dark" id="headtable">
               <tr id="red">
-                <th scope="col">Ime Pjesme</th>
-                <th scope="col">Izvođač</th>
-                <th scope="col">Album</th>
-                <th scope="col">Godina</th>
+                <th scope="col-3">Ime Pjesme</th>
+                <th scope="col-2">Izvođač</th>
+                <th scope="col-3">Album</th>
+                <th scope="col-2">Godina</th>
+                <th scope="col-1">DLT</th>
               </tr>
             </thead>
             <tbody>
@@ -105,21 +113,8 @@
   
 <script>
 
-       /*  document.querySelector('#sb').addEventListener('click',()=>
-          {
-            localStorage.setItem("saved",document.querySelector(".table").innerHTML);
-            console.log('usa');
-          })
-        
-    
-        window.addEventListener('load', 
-          function() { 
-            document.querySelector(".table").innerHTML += localStorage.saved.toString();
-        }, false); */
-     
-       
-        
-      
+
+  
 
 
 
@@ -129,10 +124,10 @@
     var album = $("#form3").val();
     var year = $("#form4").val();
     var br = $("tbody tr").length;
-    console.log(br);
+    
     
 
-    if(name == "" || artist == "" || album == "" || year < 1000){
+    if(name == "" || artist == "" || album == "" || year == 0){
       alert("podatci su neispravni ili ih niste unijeli");
     }
     else{
@@ -142,41 +137,103 @@
         $("#red"+br).append("<td>"+artist+"</td>");
         $("#red"+br).append("<td>"+album+"</td>");
         $("#red"+br).append("<td>"+year+"</td>");
+        $("#red"+br).append("<td><i class='bi bi-trash-fill'id='btntrash'></i></td>");
         $("#red"+br).append("</tr>");
-       
+
+
+        $(".bi-trash-fill").click(function(){
+          $(this).parent().parent().remove();
+          
+       }); 
         
+
         
-    }
+
+      }
 
    
   });
 
+
+  
+  
+
+
   $("#toggle").click(function(){
         
         
-      var kl =$("#toggle").attr('class');
+        var kl =$("#toggle").attr('class');
         if(kl == "bi bi-brightness-high-fill"){
-          console.log("usa");
-          $("#toggle").removeClass();
-          $("#toggle").addClass("bi bi-brightness-high");
+            console.log("usa");
+            $("#toggle").removeClass();
+            $("#toggle").addClass("bi bi-brightness-high");
+            $("body").css("background-color","#14171A");
+            $("body").css("color","white");
+            $("#navigacija").removeClass();
+            $("#navigacija").addClass("navbar navbar-expand-lg navbar-light bg-dark shadow fixed-top");
+            $("thead").removeClass();
+            $("thead").addClass("thead-light");
+            $("a").css("color","white");
+            $("input").css("border","none");
+            $("i").css("color","white");
+            $("td").css("color","white");
+
+
         }
 
         else{
-          $("#toggle").removeClass();
-          $("#toggle").addClass("bi bi-brightness-high-fill");
+            $("#toggle").removeClass();
+            $("#navigacija").removeClass();
+            $("#navigacija").addClass("navbar navbar-expand-lg navbar-light bg-light shadow fixed-top");
+            $("#mojLi").css("color","black");
+            $("#toggle").addClass("bi bi-brightness-high-fill");
+            $("body").css("background-color","white");
+            $("body").css("color","black");
+            $("thead").removeClass();
+            $("thead").addClass("thead-dark");
+            $("a").css("color","rgba(0,0,0,.55)");
+            $("input").css("border","1px solid #ced4da");
+            $("#toggle").css("color","black");
+            $("td").css("color","black");
         }
+    });
+
+
+    $("#ispis").click(function(){
+      console.log("saved");
+      
+        var array = [];
+        var headers = [];
+        $('#mojtab th').each(function(index, item) {
+            headers[index] = $(item).html();
+        });
+        $('#mojtab tr').has('td').each(function() {
+            var arrayItem = {};
+            for(i in arrayItem.length){ 
+
+              
+            }
+            $('td', $(this)).each(function(index, item) {
+                arrayItem[headers[index]] = $(item).html();
+            });
+        
         
 
+       
+            
+        });
     });
+
+     
 </script>
 <?php
-
-$ime = $_GET['in1'];
-$izv = $_GET['in2'];
-$album = $_GET['in3'];
-$godina = $_GET['in4'];
-$btn = isset($_GET['sv']);
-$isp = isset($_GET['ispis']);
+$ime = (isset($_POST['in1'])) ? $_POST['in1'] : '';
+$ime = $_POST['in1'];
+$izv = $_POST['in2'];
+$album = $_POST['in3'];
+$godina = $_POST['in4'];
+$btn = isset($_POST['sv']);
+$isp = isset($_POST['ispis']);
 
 
 if(isset($btn)){
@@ -184,6 +241,8 @@ if(isset($btn)){
   $con = mysqli_connect("localhost", "root", "", "GitaraDB");
   $ubaci = "INSERT INTO pjesme(Naslov,Izvodac,Album,Godina) values ('$ime','$izv','$album','$godina')";
   $result = mysqli_query($con, $ubaci);
+
+  
   
   
 } 
@@ -191,6 +250,7 @@ if(isset($btn)){
 if(isset($isp)){
   $con = mysqli_connect("localhost", "root", "", "GitaraDB");
   $dobavi = "SELECT * FROM pjesme";
+  echo $dobavi;
   $res = mysqli_query($con,$dobavi);
  
 
